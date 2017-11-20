520a6e8ba5511e44f821ff824315d4844a5ec0e3
Joey Mendoza <jm.mendoza@dot.ph>
2017-11-20 19:25:44 +0800

    Removed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,1 @@
-Rails.application.routes.draw do
-  mount Ahoy::Engine => "/ahoy" if Ahoy.mount
-end
 
-Ahoy::Engine.routes.draw do
-  scope module: "ahoy" do
-    resources :visits, only: [:create]
-    resources :events, only: [:create]
-  end
-end
